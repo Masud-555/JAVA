142f237288482be456b765e6ef2d5f42df8cd15d
Abdullah Al Masud <abdullahalmasud011@gmail.com>
2025-03-24 14:31:21 +0600

    Add

---
+++ 1stClass/src/pkg1stclass/Main.java
@@ -0,0 +1,17 @@
+
+package pkg1stclass;
+
+public class Main {
+  
+    public static void main(String[] args) {
+        
+        byte id =120;
+        boolean statas= true;
+        char letter = 'A';
+        int number = 9876543;
+        String userName = "Masud";
+        System.out.println("this is letter "+letter+" "+userName);
+        
+    }
+    
+}
