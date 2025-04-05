25a15339d5b16afc668757af2a917754e8a3ca79
Abdullah Al Masud <abdullahalmasud011@gmail.com>
2025-04-05 18:17:15 +0600

    add

---
+++ prac1num/src/prac1num/Prac1num.java
@@ -0,0 +1,16 @@
+
+package prac1num;
+
+
+public class Prac1num {
+
+    
+    public static void main(String[] args) {
+        
+        byte id=120;
+        short roll=4545;
+        int number=
+        
+    }
+    
+}
