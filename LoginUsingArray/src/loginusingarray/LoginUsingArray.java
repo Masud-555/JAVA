6766ef6a00d4eae9d510ccfae1b748c56e13708a
Abdullah Al Masud <abdullahalmasud011@gmail.com>
2025-04-13 19:03:05 +0600

    add

---
+++ LoginUsingArray/src/loginusingarray/LoginUsingArray.java
@@ -0,0 +1,35 @@
+
+package loginusingarray;
+
+import java.util.Scanner;
+
+
+public class LoginUsingArray {
+
+    
+    public static void main(String[] args) {
+       
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("enter user name");
+        
+        
+        String[] names = new String[n];
+        int[] pass = new int[n];
+        
+        
+        for(int i; i < names.length; i++){
+            
+            if(n == names[i].equalsIgnoreCase()){
+                 int pasword = s.nextInt();
+                 System.out.println("");
+            }
+            
+        }
+        
+        
+        
+        
+    }
+    
+}
