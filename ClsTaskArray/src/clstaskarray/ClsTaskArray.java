abbd3615ece7ffbcbe23b618cd49b4f6c92342e5
Abdullah Al Masud <abdullahalmasud011@gmail.com>
2025-04-13 17:21:46 +0600

    add

---
+++ ClsTaskArray/src/clstaskarray/ClsTaskArray.java
@@ -0,0 +1,40 @@
+
+package clstaskarray;
+
+import java.util.Arrays;
+
+
+public class ClsTaskArray {
+
+    
+    public static void main(String[] args) {
+//        
+//        String[] names = new String[5];
+//        
+//        
+//        names[0] ="Imran";
+//        names[1] ="Ishtaq";
+//        names[2] ="Atiq";
+//        names[3] ="Reja";
+//        names[4] ="Rahim";
+//        
+//        int[] marks = new int[5];
+//        
+//        marks[0] =88;
+//        marks[1] =77;
+//        marks[2] =66;
+//        marks[3] =55;
+//        marks[4] =87;
+                
+          String[] names = {"Atiq","Imran","Reza","Masud","Ishtiaq"};
+          int[] marks = {20,25,30,25,20};
+
+
+          for(int i = 0; i < names.length; i++){
+              System.out.println("name is "+names[i]+" marks is "+marks[i]);
+          }  
+          
+        
+    }
+    
+}
