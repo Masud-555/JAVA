58935357fac30bc479196420d9f7bdb5c2539f19
Masud-555 <abdullahalmasud011@gmail.com>
2025-04-23 12:01:56 +0600

    add

---
+++ LearnMethods/src/learnmethods/LearnMethods2.java
@@ -0,0 +1,23 @@
+
+package learnmethods;
+
+public class LearnMethods2 {
+    
+    public static void main(String[] args) {
+    
+        System.out.println(dosum(50, 40));
+        System.out.println(dosum(70, 30));
+        System.out.println(dosum(60, 20));
+    
+    
+    }
+    
+    public static float dosum(int num1 , float num2){
+    
+        float result = num1+num2;
+        
+        return result;
+    
+    }
+    
+}
